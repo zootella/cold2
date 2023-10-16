5d32583cb77220ca775764506a0414f700243ead
Kevin Faaborg <kevin@ara.one>
2023-10-16 14:13:06 -0400

    unified changes on three domains

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
 
 How quick, simple, and cheap can the web2 stack be in 2023?
 
-Contestants
-- 🍺 [cold1.cc](https://cold1.cc/) starting with [Amplify](https://aws.amazon.com/amplify/)
-- 🍺 [cold2.cc](https://cold1.cc/) starting with Cloudflare [pages](https://developers.cloudflare.com/pages/) and [functions](https://developers.cloudflare.com/pages/platform/functions/get-started/) or [workers](https://developers.cloudflare.com/workers/)
-- 🍺 [cold3.cc](https://cold1.cc/) starting with [SST](https://sst.dev/) on AWS
-
-Ran
-```
-$ npm create cloudflare@latest
-```
-then chose *React*
+Organized at [coldstart](https://github.com/zootella/coldstart).
--- src/App.js
+++ src/App.js
@@ -1,43 +1,71 @@
 
 import './App.css';
-
+import React from 'react';
 import { useState } from 'react';
 
-const EmailForm = () => {
+function Tick({tick}) {//tick is from Date.now()
+  if (!tick) return (<span>(not yet)</span>);//don't render jan1 1970 as a time something actually happened
 
-	const [email, setEmail] = useState("");
-	const [feedback, setFeedback] = useState("Enter text for feedback");
+  var date = new Date(tick); // Create a Date object using the timestamp
+  var weekday = date.toLocaleDateString('en-US', { weekday: 'short' });//get text like "Mon"
+  var hours = date.getHours();//extract hours, minutes, seconds, and milliseconds
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  var milliseconds = date.getMilliseconds().toString().padStart(3, "0");
 
-	const onButtonSend = e => {
-		e.preventDefault();
-		console.log(email);
-	};
+  return(<span>{weekday} {hours}h {minutes}m {seconds}.{milliseconds}s</span>);
+}
 
-	function onEmailChange(event) {
-		setFeedback(`You have entered ${event.target.value.length} characters`)
-		setEmail(event.target.value);
-	}
+const BoxForm = () => {
 
-	return (
-		<div>
-			<form onSubmit={onButtonSend}>
-				<p>
-					<label>Email: <input type="text" name="email" value={email} onChange={onEmailChange} /></label>
-					<button>Send</button>
-				</p>
-				<p>{feedback}</p>
-			</form>
-		</div>
-	);
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("no message");
+  const onMySend = e => {
+    e.preventDefault();
+    console.log(email);
+  };
+
+  function onBoxChange(event) {
+    setMessage(`measured ${event.target.value.length} characters`)
+    setEmail(event.target.value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={onMySend}>
+        <p>
+          <label>Text <input type="text" name="email" value={email} onChange={onBoxChange} /></label>{" "}
+          <button>Send</button>{" "}
+          {message}
+        </p>
+      </form>
+    </div>
+  );
 };
 
 function App() {
-	return (
-		<div className="App">
-			<p>Hello, cold2.cc! version 2023oct15b</p>
-			<EmailForm />
-		</div>
-	);
+
+  const [clicked1, setClicked1] = useState(0);
+  const [clicked2, setClicked2] = useState(0);
+
+  return (
+    <div className="App">
+      <p>
+        Loaded <Tick tick={Date.now()}/>. This is cold2.cc, on Cloudflare, version 2023oct16d.
+      </p>
+      <p>
+        Clicked <Tick tick={clicked1}/>:{" "}
+        <a href="https://cold1.cc/" target="_blank" rel="noreferrer"
+        onClick={()=>{setClicked1(Date.now())}}>cold1.cc</a>
+      </p>
+      <p>
+        Clicked <Tick tick={clicked2}/>:{" "}
+        <a href="https://cold2.cc/" target="_blank" rel="noreferrer"
+        onClick={()=>{setClicked2(Date.now())}}>cold2.cc</a>
+      </p>
+      <BoxForm />
+    </div>
+  );
 }
 
 export default App;
