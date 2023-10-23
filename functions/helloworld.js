e3f7e02fbf638f345d065f9e9d571e87e4fb1906
Kevin Faaborg <kevin@ara.one>
2023-10-23 12:23:52 -0400

    bump

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,4 +1,4 @@
 
 export function onRequest(context) {//we're given a context object about the incoming request
-	return new Response("Hello, world! Version 2023oct23a.");//and must return a Response or a promise of one
+	return new Response("Hello, world! Version 2023oct23c");//and must return a Response or a promise of one
 }
--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
   return (
     <div className="App">
       <p>
-        Loaded <Tick tick={Date.now()}/>. This is cold2.cc, on Cloudflare, version 2023oct23a.
+        Loaded <Tick tick={Date.now()}/>. This is cold2.cc, on Cloudflare, version 2023oct23c.
       </p>
       <p>
         Clicked <Tick tick={clicked1}/>:{" "}
