d86f99a28e43be8dea31f3064c7c012adb22f850
Kevin Faaborg <kevin@ara.one>
2023-10-23 11:15:10 -0400

    new version today

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,4 +1,4 @@
 
 export function onRequest(context) {//we're given a context object about the incoming request
-	return new Response("Hello, world! Version 2023oct22b.");//and must return a Response or a promise of one
+	return new Response("Hello, world! Version 2023oct23a.");//and must return a Response or a promise of one
 }
