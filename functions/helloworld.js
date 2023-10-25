740176043294d0031f3c869cac32675734950d68
Kevin Faaborg <kevin@ara.one>
2023-10-25 18:36:28 -0400

    function returns object

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,26 +1,11 @@
 
 export function onRequest(context) {//we're given a context object about the incoming request
 
-	var s = JSON.stringify(context);
-
 	var o = {};
-	o.message = "Hello, function!";
-	o.version = "Version 2023oct23i3";
+	o.requestContext = context;
+	o.message = "Hello from this cold2.cc cloudflare pages function!";
+	o.version = "Version 2023oct25a";
 	o.serverTick = Date.now();
 
 	return Response.json(o);
 }
-
-
-
-
-/*
-
-
-these work:
-
-export function onRequest(context) {//we're given a context object about the incoming request
-	return new Response("Hello, function world! Version 2023oct23i2");//and must return a Response or a promise of one
-}
-
-*/
