794e96569ab648fa913d32c76b0399ffbf0bdd9f
Kevin Faaborg <kevin@ara.one>
2023-10-24 20:19:17 -0400

    before branch

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,4 +1,26 @@
 
 export function onRequest(context) {//we're given a context object about the incoming request
-	return new Response("Hello, function world! Version 2023oct23i");//and must return a Response or a promise of one
+
+	var s = JSON.stringify(context);
+
+	var o = {};
+	o.message = "Hello, function!";
+	o.version = "Version 2023oct23i3";
+	o.serverTick = Date.now();
+
+	return Response.json(o);
 }
+
+
+
+
+/*
+
+
+these work:
+
+export function onRequest(context) {//we're given a context object about the incoming request
+	return new Response("Hello, function world! Version 2023oct23i2");//and must return a Response or a promise of one
+}
+
+*/
