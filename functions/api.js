7136e6a5e36793400fb8aa0842375fefd31cc9c9
Kevin Faaborg <kevin@ara.one>
2023-11-23 17:09:31 -0700

    forgot to push

--- functions/api.js
+++ functions/api.js
@@ -1,12 +1,25 @@
 
 export function onRequest(context) {//we're given a context object about the incoming request
 
-	var o = {};
-	o.requestContext = context;
-	o.message = "Hello from this cold2.cc cloudflare pages function!";
-	o.secretLength = (context.env.MY_FIRST_SECRET) ? context.env.MY_FIRST_SECRET.length : 0;
-	o.version = "Version 2023nov5k";
-	o.serverTick = Date.now();
 
-	return Response.json(o);
+	let i = context.request.headers.entries();
+	let o = {};
+	let duplicate = false;
+	for (const [k, v] of i) {
+		if (!o[k])
+			o[k] = v;
+		else
+			duplicate = true;
+	}
+
+	let o2 = {};
+	o2.requestContext = context;
+	o2.message = "Hello from this cold2.cc cloudflare pages function!";
+	o2.secretLength = (context.env.MY_FIRST_SECRET) ? context.env.MY_FIRST_SECRET.length : 0;
+	o2.version = "Version 2023nov5k5";
+	o2.serverTick = Date.now();
+	o2.headers = o;
+	o2.headersDuplicate = duplicate;
+
+	return Response.json(o2);
 }
